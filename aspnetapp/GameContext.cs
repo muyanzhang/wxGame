71a38d7e56f16cb3b28dc58fc3264c1aac19cb02
muyanzhang <136299926+muyanzhang@users.noreply.github.com>
2024-12-24 21:57:57 +0800

    1

--- aspnetapp/GameContext.cs
+++ aspnetapp/GameContext.cs
@@ -8,6 +8,20 @@ public partial class GameContext : DbContext
 
     public DbSet<User> Users { get; set; } = null!;
     public DbSet<GameData> GameData { get; set; } = null!;
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var username = Environment.GetEnvironmentVariable("MYSQL_USERNAME");
+            var password = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
+            var addressParts = Environment.GetEnvironmentVariable("MYSQL_ADDRESS")?.Split(':');
+            var host = addressParts?[0];
+            var port = addressParts?[1];
+            var connstr = $"server={host};port={port};user={username};password={password};database=aspnet_demo";
+            optionsBuilder.UseMySql(connstr, Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.18-mysql"));
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- aspnetapp/Program.cs
+++ aspnetapp/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<CounterContext>();
+// builder.Services.AddDbContext<CounterContext>();
 builder.Services.AddDbContext<GameContext>();
 
 var app = builder.Build();
