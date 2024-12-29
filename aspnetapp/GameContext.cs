bc7624b1555ff5a666ca92de50c0677c46c1e564
muyanzhang <136299926+muyanzhang@users.noreply.github.com>
2024-12-29 14:13:04 +0800

    1

--- aspnetapp/GameContext.cs
+++ aspnetapp/GameContext.cs
@@ -18,7 +18,7 @@ public partial class GameContext : DbContext
             var addressParts = Environment.GetEnvironmentVariable("MYSQL_ADDRESS")?.Split(':');
             var host = addressParts?[0];
             var port = addressParts?[1];
-            var connstr = $"server={host};port={port};user={username};password={password};database=aspnet_demo";
+            var connstr = $"server={host};port={port};user={username};password={password};database=game";
             optionsBuilder.UseMySql(connstr, Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.18-mysql"));
         }
     }
